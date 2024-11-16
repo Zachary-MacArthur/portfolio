c808ba220319fb772ed4458c392dfe90e7c4bc20
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-11-16 08:17:45 -0500

    5

--- src/components/commands/Projects.tsx
+++ src/components/commands/Projects.tsx
@@ -9,7 +9,7 @@ const Projects: React.FC = () => {
       <EduIntro>  <p>selected work</p></EduIntro>
       <EduList>
         <p> <a className="desc" href="" target="_blank">self-hosted media player ~ coming soon</a></p>
-        <p> <a className="desc" href="https://blog-q1a5.onrender.com/" target="_blank">personal blog</a></p>
+        <p> <a className="desc" href="https://blog-q1a5.onrender.com/" target="_blank">..blog</a></p>
         <p> <a className="desc" href="https://github.com/Zachary-MacArthur/SuperCollider" target="_blank">super collider patches</a></p>
         <p> <a className="desc" href="https://cure-005.netlify.app/" target="_blank">cure 005 </a></p>
 
